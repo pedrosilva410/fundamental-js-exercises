8dab663ef7bd420d1b9be0aa2d1a8a88c664124b
Pedro Silva <pedrogdsilva410@gmail.com>
2025-01-04 23:14:31 +0100

    fix ex1

--- variables.js
+++ variables.js
@@ -1,6 +1,13 @@
-let string = "";
+let string = "foo";
 let number = 1;
-let boolean = true; 
+let boolean = true;
 let nullvar = null;
 let undfnvar = undefined;
-let symbol = ?;
+const sym = Symbol("foo");
+
+console.log(string);
+console.log(number);
+console.log(boolean);
+console.log(nullvar);
+console.log(undfnvar);
+console.log(sym);
